9276a49eedd9185a69cdfd3a517a4294f286ef0c
ashwin <ashwin@usistech.com>
2019-07-18 07:35:29 +0530

    Initial page design

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>GK App</h1>
+<p>G.K Quiz app backed with Ionic</p>
+
---
+++ src/app/page/menu/menu.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MenuPage } from './menu.page';
+
+
+
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo : '/menu/main',
+    pathMatch : 'full'
+  },
+  {
+    path : '',
+    component : MenuPage,
+    children : [
+      {
+        path : 'main',
+        loadChildren : '../details/details.module#DetailsPageModule'
+      },
+      {
+        path : 'list',
+        loadChildren : '../list/list.module#ListPageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MenuPage]
+})
+export class MenuPageModule {}
